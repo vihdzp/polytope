a274814eb7bde714ba709e277e3e76ef58e42afb
Violeta Hernández <vi.hdz.p@gmail.com>
2021-07-23 19:20:03 -0500

    Proved `flag_grades_Iic`

--- src/polytope.lean
+++ src/polytope.lean
@@ -1,4 +1,4 @@
-import set_theory.ordinal order.bounded_lattice order.zorn data.set.intervals.ord_connected order.rel_iso
+import tactic set_theory.ordinal order.bounded_lattice order.zorn data.set.intervals.ord_connected order.rel_iso
 
 open_locale cardinal
 
@@ -50,7 +50,7 @@ set_option old_structure_cmd false
 
 namespace abstract_polytope
 
-variables {α : Type*} [graded_bounded_partial_order α] (f f' : set α)
+variables {α : Type*} [graded_bounded_partial_order α] {f f' : set α}
 
 -- A flag is a maximal chain.
 def is_flag (c : set α) : Prop :=
@@ -61,7 +61,7 @@ def flag_adj : Prop :=
 is_flag f → is_flag f' → #(set.diff f f') = 1
 
 -- In a flag, `grade a < grade b` implies `a < b`.
-theorem flag_grade_lt_of_lt (a b : α) (ha : a ∈ f) (hb : b ∈ f): is_flag f → graded.grade a < graded.grade b → a < b := 
+theorem flag_grade_lt_of_lt {a b : α} (ha : a ∈ f) (hb : b ∈ f): is_flag f → graded.grade a < graded.grade b → a < b := 
 begin
   intros ff ga_lt_gb,
   cases ff with ch _,
@@ -92,59 +92,142 @@ variables {α : Type*} [abstract_polytope α] (f f' : set α)
 
 def grade : α → ℕ := graded.grade
 
--- Any flag contains the bottom element `⊥`.
-theorem bot_in_flag : is_flag f → ⊥ ∈ f :=
+-- If one attempts to extend a flag `f` by an element `e` which is comparable to
+-- all other faces of the flag, we obtain a contradiction. 
+lemma flag_extend (e : α) : is_flag f → e ∉ f → ¬(∀ a ∈ f, a ≠ e → a < e ∨ e < a) :=
 begin
-  -- We add `⊥` to the flag. 
-  intro ff,
-  by_contra,
-  let f' : set α := f ∪ {⊥},
-  have hf' : ∀ a ∈ f', a ∈ f ∨ a = ⊥ := by simp,
+  -- We define `f' = f ∪ {e}`.
+  intros ff e_nmem_f h,
+  let f' := f ∪ {e},
+  have hf' : ∀ a ∈ f', a ∈ f ∨ a = e := by simp,
+  apply ff.right,
+  use f',
     
-  -- f is not equal to f'.
+  -- `f` is not equal to `f'`.
   have f_ne_f' : f ≠ f' := begin
     intro f_eq_f',
-    rw f_eq_f' at h,
-    exact h (set.mem_union_right f rfl),
+    rw f_eq_f' at e_nmem_f,
+    exact e_nmem_f (set.mem_union_right f rfl),
   end,
+  
+  -- We prove that `f'` is a super chain of `f`, a contradiction!
+  split, {
+    intros a a_mem_f' b b_mem_f' a_ne_b,
 
-  -- f is a strict subset of f'.
-  have f_in_f' : f ⊂ f' := set.ssubset_iff_subset_ne.mpr ⟨set.subset_union_left f {⊥}, f_ne_f'⟩,
-
-  -- We prove that `f'` is a super chain of `f`.
-  have h₁ : @zorn.super_chain _ (<) f f' := begin
-    split, {
-      intros a af' b bf' a_ne_b,
-
-      -- Cases depending on whether `a` or `b` equal `⊥`. 
-      cases hf' a af' with af a_eq_t, {
-        cases hf' b bf' with bf b_eq_t, {
-          exact ff.left a af b bf a_ne_b,
-        },
-        rw b_eq_t at *,
-        exact or.inr ((ne.symm a_ne_b).le_iff_lt.mp bot_le),        
+    -- Cases depending on whether `a` or `b` equal `e`. 
+    cases hf' a a_mem_f' with a_mem_f a_eq_t, {
+      cases hf' b b_mem_f' with b_mem_f b_eq_t, {
+        exact ff.left a a_mem_f b b_mem_f a_ne_b,
       },
-      rw a_eq_t at *,
-      exact or.inl (bot_lt_iff_ne_bot.mpr (ne.symm a_ne_b)),      
+      rw b_eq_t at *,
+      exact h a a_mem_f a_ne_b,
     },
+    cases hf' b b_mem_f' with b_mem_f b_eq_t, {
+      rw a_eq_t at *,
+      exact or.swap (h b b_mem_f (ne.symm a_ne_b)),   
+    },
+    rw b_eq_t at *,
+    exact false.elim (a_ne_b a_eq_t),
+  },
 
-    exact f_in_f',
-  end,
-
-  -- We then prove that `f` is not a max chain.
-  have h₂ : ¬ @zorn.is_max_chain _ (<) f := begin
-    by_contra, exact h.right ⟨f', h₁⟩,
-  end,
-
-  -- But this contradicts the definition of a flag.
-  exact h₂ ff,
+  exact set.ssubset_iff_subset_ne.mpr ⟨set.subset_union_left f {e}, f_ne_f'⟩,
 end
 
+-- Any flag contains the bottom face `⊥`.
+theorem bot_in_flag : is_flag f → ⊥ ∈ f :=
+begin
+  -- We use the `flag_extend` lemma.
+  intro ff,
+  by_contra bot_nmem_f,
+  apply flag_extend f ⊥ ff bot_nmem_f,
+
+  -- The bottom face is less or equal to any other.
+  intros a a_mem_f a_ne_bot,
+  exact or.inr (lt_of_le_of_ne bot_le (ne.symm a_ne_bot)),
+end
+
+-- Any flag contains the top face `⊤`.
 theorem top_in_flag : is_flag f → ⊤ ∈ f :=
-sorry,
+begin
+  -- We use the `flag_extend` lemma.
+  intro ff,
+  by_contra bot_nmem_f,
+  apply flag_extend f ⊤ ff bot_nmem_f,
+
+  -- The top face is greater or equal to any other.
+  intros a a_mem_f a_ne_bot,
+  exact or.inl (lt_of_le_of_ne le_top (a_ne_bot)),
+end
 
 -- The set of grades of a flag.
 def flag_grades : set ℕ := {n | ∃ a ∈ f, grade a = n}
+
+-- If a flag contains `a` and `c` but no elements in between, `grade a + 1 = grade c`.
+-- This strengthens `eq_p1_of_covers`.
+lemma flag_eq_p1_of_covers (a c : α) (a_mem_f : a ∈ f) (c_mem_f : c ∈ f) : is_flag f → a < c → (¬ ∃ b ∈ f, a < b ∧ b < c) → grade a + 1 = grade c :=
+begin
+  -- It suffices to prove that `a` covers `c`.
+  intros ff a_lt_c hne,
+  apply graded.eq_p1_of_cover,  
+  use flag_grade_lt_of_lt a_mem_f c_mem_f ff (graded.lt_grade_of_lt a_lt_c),
+  
+  -- To do this, we prove that any element between `a` and `c` must be in `f`.
+  by_contra he,
+  cases he with b a_mem_ibc,
+  apply hne,
+  use b,
+  split, {
+    -- To use the `flag_extend` lemma, we must prove that any `x ∈ f` is
+    -- comparable to `b`.
+    by_contra b_nmem_f,
+    apply flag_extend f b ff b_nmem_f,
+    intros x x_mem_f _,
+
+    -- We use that flags are total orders.
+    have nle_of_lt : ∀ a b ∈ f, ¬ b ≤ a → a < b := begin
+      intros a b a_mem_f b_mem_f h,
+
+      have a_ne_b : a ≠ b := begin
+        intro a_eq_b,
+        rw a_eq_b at h,
+        exact h rfl.ge,
+      end,
+
+      have a_nge_b : ¬ a > b := begin
+        intro a_gt_b,
+        exact h (le_of_lt a_gt_b),
+      end,
+
+      cases ff.left a a_mem_f b b_mem_f a_ne_b with ra rb, {
+        exact ra,
+      },
+      exact false.elim (a_nge_b rb),
+    end,
+
+    -- `x` must be below `a` or above `c` (since there's no elements in between).
+    have x_le_a_or_c_le_x : x ≤ a ∨ c ≤ x := begin
+      by_cases x_le_a : x ≤ a, {
+        exact or.inl x_le_a,
+      },
+      by_cases c_le_x : c ≤ x, {
+        exact or.inr c_le_x,
+      },
+      
+      by_contra,
+      have a_lt_x := nle_of_lt a x a_mem_f x_mem_f x_le_a,
+      have x_lt_c := nle_of_lt x c x_mem_f c_mem_f c_le_x,
+      exact hne ⟨x, x_mem_f, a_lt_x, x_lt_c⟩,
+    end,
+
+    -- We finish by transitivity.
+    cases x_le_a_or_c_le_x with x_le_a c_le_x, {
+      exact or.inl (lt_of_le_of_lt x_le_a a_mem_ibc.left),
+    },
+    exact or.inr (lt_of_lt_of_le a_mem_ibc.right c_le_x),
+  }, 
+
+  exact a_mem_ibc,  
+end
 
 -- A flag contains faces of each grade up to the grade of its topmost face.
 theorem flag_grades_Iic : is_flag f → flag_grades f = set.Iic (grade (⊤ : α)) :=
@@ -154,7 +237,7 @@ begin
   let N := grade (⊤ : α),
   let I := set.Iic N,
 
-  -- Every flag grade is between 0 and N.
+  -- Every flag grade is between `0` and `N`.
   have G_in_I : G ⊆ I := begin
     intros _ g_mem_G,
     cases g_mem_G with a ha, 
@@ -164,7 +247,7 @@ begin
     exact ga_lt_N,
   end,
 
-  -- Every number between 0 and N is a flag grade.
+  -- Every number between `0` and `N` is a flag grade.
   have I_in_G : I ⊆ G := begin
     -- We suppose, by contradiction, that we're missing a number `n`.
     intros n n_mem_I,
@@ -194,14 +277,14 @@ begin
       exact n_nmem_G m_mem_G,
     end,
 
-    -- We prove that no grades in (m, n) may appear in G.
+    -- We prove that no grades in `(m, n)` may appear in `G`.
     have hm : ∀ k : ℕ, k ∈ set.Ioo m n → k ∉ G := begin
       intros k k_mem_i,
-      by_contra,
+      by_contra k_mem_G,
       have k_le_m : k ≤ m := begin
         apply finset.le_max',
         apply set.mem_to_finset.mpr,
-        exact set.mem_sep (set.mem_Iic.mpr (le_of_lt k_mem_i.right)) h,
+        exact set.mem_sep (set.mem_Iic.mpr (le_of_lt k_mem_i.right)) k_mem_G,
       end,
       exact false.elim (not_lt.mpr k_le_m k_mem_i.left),
     end,
@@ -224,18 +307,69 @@ begin
       exact n_nmem_G M_mem_G,
     end,
 
-    -- We prove that no grades in (n, M) may appear in G.
-    have hm : ∀ k : ℕ, k ∈ set.Ioo n M → k ∉ G := begin
+    -- We prove that no grades in `[n, M)` may appear in `G`.
+    have hM : ∀ k : ℕ, k ∈ set.Ico n M → k ∉ G := begin
       intros k k_mem_i,
-      by_contra,
-      simp at k_mem_i,
+      by_contra k_mem_G,
       have k_ge_m : k ≥ M := begin
-        sorry,
+        apply le_of_not_lt,
+        apply well_founded.not_lt_min nat.lt_wf SM SM_non,
+        exact set.mem_sep (set.mem_Ici.mpr k_mem_i.left) k_mem_G,
       end,
       exact false.elim (not_lt.mpr k_ge_m k_mem_i.right),
     end,
+
+    -- `m < M`, obviously.
+    have m_lt_M : m < M := lt_trans m_lt_n n_lt_M,
     
-    sorry,
+    -- We build faces `a` and `c` in the flag with grades `m` and `M`.
+    cases m_mem_G with a ea,
+    cases M_mem_G with c ec,
+    cases ea with a_mem_f ga_eq_m,
+    cases ec with c_mem_f gc_eq_M,
+
+    -- `a` must be less than `c`.
+    have ga_eq_m : grade a = m := ga_eq_m,
+    have gc_eq_M : grade c = M := gc_eq_M,
+    have a_lt_c : a < c := begin
+      rw ←ga_eq_m at m_lt_M,
+      rw ←gc_eq_M at m_lt_M,
+      exact flag_grade_lt_of_lt a_mem_f c_mem_f ff m_lt_M,
+    end,
+
+    -- There can't exist any face in the flag that's between `a` and `c`.
+    have h : ¬ ∃ b ∈ f, a < b ∧ b < c := begin
+      intro he,
+      cases he with b he,
+      cases he with b_mem_f a_lt_b_and_b_lt_c,
+      cases a_lt_b_and_b_lt_c with a_lt_b b_lt_c,
+
+      let g := grade b,
+      have m_lt_g : m < g := begin
+        rw ←ga_eq_m,
+        exact graded.lt_grade_of_lt a_lt_b,
+      end,
+      have g_lt_M : g < M := begin
+        rw ←gc_eq_M,
+        exact graded.lt_grade_of_lt b_lt_c,
+      end,
+      have g_mem_G : g ∈ G := ⟨b, b_mem_f, rfl⟩,
+      by_cases g_lt_n : g < n, {
+        exact hm g (set.mem_inter m_lt_g g_lt_n) g_mem_G,
+      },
+      exact hM g (set.mem_inter (le_of_not_gt g_lt_n) g_lt_M) g_mem_G,
+    end,
+
+    -- As a consequence, `m + 1 = M`.
+    have ga_p1_eq_gc : grade a + 1 = grade c := flag_eq_p1_of_covers f a c a_mem_f c_mem_f ff a_lt_c h,
+    have m_p1_eq_M : m + 1 = M := begin
+      rw ga_eq_m at ga_p1_eq_gc,
+      rw gc_eq_M at ga_p1_eq_gc,
+      exact ga_p1_eq_gc,
+    end,
+
+    -- But then, the existence of `n` such that `m < n < M` is impossible!
+    linarith,
   end,
 
   exact set.subset.antisymm G_in_I I_in_G,
@@ -341,3 +475,5 @@ begin
   exact h,
 end
 end abstract_polytope
+
+#lint
